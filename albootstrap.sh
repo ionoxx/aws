60b619a0dbfc6ba1f7f07b5236725df6868a53cb
Chartier <pchartie@amazon.com>
2023-06-16 09:44:09 -0400

    first commit

---
+++ albootstrap.sh
@@ -0,0 +1,11 @@
+/usr/bin/yum upgrade -y
+/usr/bin/yum install httpd -y
+/usr/bin/systemctl enable httpd
+/usr/bin/needs-restarting  -r
+if [ $? -eq 1 ]
+then
+echo "Reboot Required"
+  /usr/sbin/reboot"
+else
+echo "Reboot Not Required"
+fi
