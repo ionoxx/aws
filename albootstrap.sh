61d323c7bb6a67fdeba605ebb94ad413ec21b1be
Chartier <pchartie@amazon.com>
2023-06-16 10:56:03 -0400

    fixed reboot

--- albootstrap.sh
+++ albootstrap.sh
@@ -2,11 +2,4 @@
 /usr/bin/yum install httpd -y
 /usr/bin/systemctl enable httpd
 /usr/bin/systemctl start httpd
-/usr/bin/needs-restarting  -r
-if [ $? -eq 1 ]
-then
-echo "Reboot Required"
-  /usr/sbin/reboot"
-else
-echo "Reboot Not Required"
-fi
+/usr/sbin/reboot
